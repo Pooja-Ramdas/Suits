603a992731e83e9b64f84cd6951de765cb676ec5
rithikaaredde <rithikaaredde0103@gmail.com>
2025-03-05 19:03:00 +0530

    Added getters and fucntions to check for same suit and rank

--- Card.java
+++ Card.java
@@ -1,18 +1,37 @@
-// Card. java
+// Card.java
 
-class Card {
-	String rank;
-	String suit;
-	
-	public Card(String rank, String suit)
-	{
-		this.rank = rank;
-		this.suit = suit;
-	}
-	
-	public String toString()
-	{
-		return rank + " of " + suit;
-	}
-	
+// Represents a single playing card
+public class Card {
+    private String rank;
+    private String suit;
+
+    // Constructor
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    // Getters
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    // String representation of a card
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
+    // Check if two cards have the same suit
+    public boolean sameSuit(Card other) {
+        return this.suit.equals(other.suit);
+    }
+
+    // Check if two cards have the same rank
+    public boolean sameRank(Card other) {
+        return this.rank.equals(other.rank);
+    }
 }
